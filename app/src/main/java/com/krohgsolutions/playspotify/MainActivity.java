6d1af3b7084f653c0ddca89ee39d1945bf249601
alexkrohg64 <alexkrohg@gmail.com>
2019-02-05 09:43:45 -0500

    Initial commit

---
+++ app/src/main/java/com/krohgsolutions/playspotify/MainActivity.java
@@ -0,0 +1,108 @@
+package com.krohgsolutions.playspotify;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String CLIENT_ID = "fee2c0b9aebc4e4a838507645af61f25";
+    private static final String REDIRECT_URI = "spotify-app-remote://callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
+    // Request code will be used to verify if result comes from the login activity. Can be set to any integer.
+    private static final int REQUEST_CODE = 1337;
+    private static String AUTH_TOKEN = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        AuthenticationRequest.Builder builder =
+                new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+
+        builder.setScopes(new String[]{"streaming"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    private void initializePlayer() {
+        // Set the connection parameters
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+
+                    @Override
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("MainActivity", "Connected to Player");
+
+                        new PerformSearchTask().execute("");
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.e("MainActivity", throwable.getMessage(), throwable);
+
+                        // Something went wrong when attempting to connect! Handle errors here
+                    }
+                });
+    }
+
+    protected static String getAuthToken() { return AUTH_TOKEN; }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    // Handle successful response
+                    AUTH_TOKEN = response.getAccessToken();
+                    Log.d("MainActivity","Successfully received Auth Token");
+                    initializePlayer();
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    // Handle error response
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/krohgsolutions/playspotify/PerformSearchTask.java
@@ -0,0 +1,48 @@
+package com.krohgsolutions.playspotify;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+class PerformSearchTask extends AsyncTask<String, Integer, String> {
+    protected String doInBackground(String... urls) {
+        try {
+            Log.d("MainActivity","Executing Search for Random Track");
+            URL url = new URL("https://api.spotify.com/v1/search/?q=name:gold&type=track&market=from_token");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty ("Authorization", MainActivity.getAuthToken());
+            connection.setRequestMethod("GET");
+            connection.connect();
+            Log.d("MainActivity", connection.getResponseCode() + "");
+            BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String content = "";
+            String line = null;
+            while ((line = rd.readLine()) != null) {
+                content += line + "\n";
+            }
+
+            return content;
+
+        } catch (Exception e) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            Log.e("MainActivity", sw.toString());
+
+            return "";
+        }
+    }
+
+    protected void onProgressUpdate(Integer... progress) {
+    }
+
+    protected void onPostExecute(String result) {
+        return;
+    }
+}
