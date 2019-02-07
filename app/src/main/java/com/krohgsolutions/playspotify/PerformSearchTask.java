2d0abc3219d115386d2e59df4d5449de1c7b7001
alexkrohg64 <alexkrohg@gmail.com>
2019-02-06 21:24:30 -0500

    Change ThreadLocalRandom for Random

--- app/src/main/java/com/krohgsolutions/playspotify/PerformSearchTask.java
+++ app/src/main/java/com/krohgsolutions/playspotify/PerformSearchTask.java
@@ -9,13 +9,13 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
 
 class PerformSearchTask extends AsyncTask<String, Integer, String> {
     protected String doInBackground(String... urls) {
         try {
             Log.d("PerformSearchTask","Executing Search for Random Track");
-            int offset = ThreadLocalRandom.current().nextInt(0, 10000);
+            int offset = new Random().nextInt(10000);
             URL url = new URL("https://api.spotify.com/v1/search/?q=name:*&type=track&market=from_token&limit=1&offset=" + offset);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestProperty ("Authorization", "Bearer " + MainActivity.getAuthToken());
